9b25524480d54fdd061a7c142005d73f9bab651d
Brian Palay <bpplaysguitar@hotmail.com>
2023-12-10 11:38:18 -0600

    new

--- src/pages/Project4.js
+++ src/pages/Project4.js
@@ -16,29 +16,40 @@ function Project4() {
         <div className="container my-4" id="">
           <div className="col">
             <p className="my-4">
-              I am working on completing the{" "}
+              This is a project I made during the{" "}
               <a
                 target="_blank"
                 href="https://www.coursera.org/professional-certificates/google-ux-design"
               >
-                Google UX Design Professional Certificate on Coursera
-              </a>
-              . It has been very fun and extremely educational for me as a
-              designer. I am halfway through course 5 of 7. I have been
-              designing an app for a sandwich shop, from paper wireframes to a
-              finished product, including UX research with real users. When the
-              project is completed, there will be an entire slide deck of it
-              here, showing every stage of the design process, but for now, feel
-              free to interact with my prototype I made in Figma.
+                Google UX Design Professional Certificate
+              </a>{" "}
+              course on Coursera. I designed a sandwich shop app. The process
+              took me through many phases of UX design, inlcuding paper
+              wireframes, low-fidelity wireframes in Figma, high-fiedelity
+              wireframes in Figma, prototyping, and user testing. Please seee
+              the slide deck for more details on this project.
             </p>
-            <p className="my-4">
+            <div className="iframe my-5">
+              <iframe
+                src="https://docs.google.com/presentation/d/e/2PACX-1vRwcE5aXHoV9-LGhGDmLxmEPtE7O4vkRhCwGztfyG2Qvsj31NhexSqXbR-vE_mc4OqWaru8T6a44X7A/embed?start=false&loop=false&delayms=15000"
+                className="d-block mx-auto shadow border border-3 border-dark mb-3 rounded"
+                frameborder="0"
+                width="960"
+                height="569"
+                allowfullscreen="true"
+                mozallowfullscreen="true"
+                webkitallowfullscreen="true"
+              ></iframe>
+            </div>
+            <p className="my-5 mx-auto text-center">
               <a
                 target="_blank"
-                href="https://www.figma.com/proto/dCpvMiEMaJPJiw4HYDNvwk/Portfolio-Wireframes-UPDATED-v3?type=design&node-id=109-408&t=LvdSYcdk0uc65ivK-1&scaling=scale-down&page-id=0%3A1&starting-point-node-id=109%3A408&mode=design"
+                href="https://docs.google.com/presentation/d/1Lpl2apYXFdKpum3LJr-YnqZx3b1hFd0bsE3ZfXWDG2E/edit?usp=sharing"
               >
-                Interact with the Figma Prototype
+                View Google Slides deck
               </a>
             </p>
+
             <p className="my-4">
               <a
                 target="_blank"
@@ -49,6 +60,14 @@ function Project4() {
                   alt="Mobile phone frame displaying screenshot of sandwich shop app prototype"
                   src={prototypePreview}
                 />
+              </a>
+            </p>
+            <p className="my-3 mx-auto text-center">
+              <a
+                target="_blank"
+                href="https://www.figma.com/proto/dCpvMiEMaJPJiw4HYDNvwk/Portfolio-Wireframes-UPDATED-v3?type=design&node-id=109-408&t=LvdSYcdk0uc65ivK-1&scaling=scale-down&page-id=0%3A1&starting-point-node-id=109%3A408&mode=design"
+              >
+                Interact with the Figma Prototype
               </a>
             </p>
           </div>
